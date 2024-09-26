9e74e3b1da7e8e1746bfc40209d16e4aeb762c8d
TUGITA <tugitaa@gmail.com>
2024-09-26 12:05:18 +0000

    doSpaceService

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+//bot.js
 const fs = require('fs');
 const path = require('path');
 const app = require('./server');
@@ -22,35 +23,38 @@ bot.onText(/\/start/, async (msg) => {
             // Получаем файл последнего фото профиля
             const fileId = userProfilePhotos.photos[0][0].file_id;
 
-            // Запрашиваем путь к файлу через Telegram API
-            const filePathResponse = await axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/getFile`, {
-                params: { file_id: fileId }
-            });
+            try {
+                // Запрашиваем путь к файлу через Telegram API
+                const filePathResponse = await axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/getFile`, {
+                    params: { file_id: fileId }
+                });
 
-            const filePath = filePathResponse.data.result.file_path;
+                const filePath = filePathResponse.data.result.file_path;
 
-            // Получаем изображение по file_path
-            const fileResponse = await axios.get(`https://api.telegram.org/file/bot${BOT_TOKEN}/${filePath}`, {
-                responseType: 'arraybuffer'
-            });
+                // Получаем изображение по file_path
+                const fileResponse = await axios.get(`https://api.telegram.org/file/bot${BOT_TOKEN}/${filePath}`, {
+                    responseType: 'arraybuffer'
+                });
 
-            const fileBuffer = Buffer.from(fileResponse.data, 'binary');
+                const fileBuffer = Buffer.from(fileResponse.data, 'binary');
 
-            // Сохраняем оригинальное фото профиля
-            console.log('Сохранение фото профиля пользователя...');
-            const fileUrl = await uploadOriginalFile(chatId, {
-                buffer: fileBuffer,
-                mimetype: 'image/jpeg',
-                originalname: `${chatId}.jpg`, // Устанавливаем имя файла
-            });
+                // Сохраняем оригинальное фото профиля на CDN
+                console.log('Сохранение фото профиля пользователя на CDN...');
+                const fileUrl = await uploadOriginalFile(chatId, {
+                    buffer: fileBuffer,
+                    mimetype: 'image/jpeg',
+                    originalname: `${chatId}.jpg`, // Устанавливаем имя файла
+                });
 
-            console.log(`Фото профиля пользователя ${chatId} успешно сохранено. URL: ${fileUrl}`);
+                console.log(`Фото профиля пользователя ${chatId} успешно сохранено. URL: ${fileUrl}`);
+            } catch (error) {
+                console.error(`Ошибка при получении или сохранении фото профиля для пользователя ${chatId}:`, error.message);
+            }
         } else {
             console.log(`Пользователь ${chatId} не имеет фото профиля.`);
         }
 
         // Отправляем сообщение с кнопкой "Start App"
-        console.log('Отправка сообщения с кнопкой "Start App"...');
         await bot.sendMessage(chatId, 'Hello, friend! Head over to the miniapp and create your pixel avatar.', {
             reply_markup: {
                 inline_keyboard: [
@@ -62,7 +66,6 @@ bot.onText(/\/start/, async (msg) => {
             }
         });
 
-        console.log('Сообщение с кнопкой "Start App" успешно отправлено.');
     } catch (error) {
         console.error('Ошибка при обработке команды /start:', error);
         await bot.sendMessage(chatId, 'Произошла ошибка при обработке вашего запроса.');
