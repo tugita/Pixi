9ff10a4f1502d37a1e3527a89fb42cdfae68f2ff
exerbaind <digr98@gmail.com>
2024-09-26 17:31:34 +0400

    init

--- public/app.js
+++ public/app.js
@@ -3,211 +3,222 @@ Telegram.WebApp.ready();
 window.Telegram.WebApp.expand();
 window.Telegram.WebApp.disableVerticalSwipes();
 
-const userId = Telegram.WebApp.initDataUnsafe.user.id;
-const firstName = Telegram.WebApp.initDataUnsafe.user.first_name || 'User';
+// const userId = Telegram.WebApp.initDataUnsafe.user.id;
+// const firstName = Telegram.WebApp.initDataUnsafe.user.first_name || 'User';
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const pixelSizeInput = document.getElementById('pixel-size');
-const pixelSizeValue = document.getElementById('pixel-size-value');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const pixelSizeInput = document.getElementById("pixel-size");
+const pixelSizeValue = document.getElementById("pixel-size-value");
 let imageLoaded = false;
 let img = new Image();
 
-canvas.style.borderRadius = '50%';
+canvas.style.borderRadius = "50%";
 
 // Предотвращаем перетаскивание и скроллинг
 function preventDefaultTouch(event) {
-    event.preventDefault();
+  event.preventDefault();
 }
 
-canvas.addEventListener('touchstart', preventDefaultTouch, { passive: false });
-canvas.addEventListener('touchmove', preventDefaultTouch, { passive: false });
+canvas.addEventListener("touchstart", preventDefaultTouch, { passive: false });
+canvas.addEventListener("touchmove", preventDefaultTouch, { passive: false });
 
-document.addEventListener('scroll', function() {
+document.addEventListener(
+  "scroll",
+  function () {
     window.scrollTo(0, 0);
-}, { passive: false });
+  },
+  { passive: false }
+);
 
 // Сохраняем фото в локальное хранилище
 function savePhotoToLocal(photoBlob) {
-    const reader = new FileReader();
-    reader.onloadend = function() {
-        localStorage.setItem('profilePhoto', reader.result); // Сохраняем фото в base64
-    };
-    reader.readAsDataURL(photoBlob);
+  const reader = new FileReader();
+  reader.onloadend = function () {
+    localStorage.setItem("profilePhoto", reader.result); // Сохраняем фото в base64
+  };
+  reader.readAsDataURL(photoBlob);
 }
 
 // Загружаем фото из локального хранилища
 function loadPhotoFromLocal() {
-    const photoBase64 = localStorage.getItem('profilePhoto');
-    if (photoBase64) {
-        img.src = photoBase64;
-        img.onload = () => {
-            canvas.style.borderRadius = '50%';
-            drawImageToCanvas(img);
-        };
-    } else {
-        console.error('Фото профиля не найдено в локальном хранилище');
-        return false; // Возвращаем false, если фото нет
-    }
-    return true; // Фото загружено успешно
+  const photoBase64 = localStorage.getItem("profilePhoto");
+  if (photoBase64) {
+    img.src = photoBase64;
+    img.onload = () => {
+      canvas.style.borderRadius = "50%";
+      drawImageToCanvas(img);
+    };
+  } else {
+    console.error("Фото профиля не найдено в локальном хранилище");
+    return false; // Возвращаем false, если фото нет
+  }
+  return true; // Фото загружено успешно
 }
 
 // Загрузка изображения с сервера и сохранение в локальное хранилище
 async function loadImageFromServer() {
-    try {
-        const response = await fetch(`/showProfilePhoto?userId=${userId}`);
-        if (!response.ok) {
-            throw new Error('Ошибка при получении фото профиля');
-        }
-
-        const blob = await response.blob();
-        savePhotoToLocal(blob); // Сохраняем в локальное хранилище
-
-        const url = URL.createObjectURL(blob);
-
-        await new Promise((resolve) => {
-            img.src = url;
-            img.onload = () => {
-                canvas.style.borderRadius = '50%';
-                resolve();
-            };
-        });
-
-        drawImageToCanvas(img);
-    } catch (error) {
-        console.error('Ошибка при получении фото профиля с сервера:', error);
-        document.getElementById('profile-photo').innerText = 'Произошла ошибка при получении фото профиля.';
+  try {
+    const response = await fetch(`/showProfilePhoto?userId=${userId}`);
+    if (!response.ok) {
+      throw new Error("Ошибка при получении фото профиля");
     }
+
+    const blob = await response.blob();
+    savePhotoToLocal(blob); // Сохраняем в локальное хранилище
+
+    const url = URL.createObjectURL(blob);
+
+    await new Promise((resolve) => {
+      img.src = url;
+      img.onload = () => {
+        canvas.style.borderRadius = "50%";
+        resolve();
+      };
+    });
+
+    drawImageToCanvas(img);
+  } catch (error) {
+    console.error("Ошибка при получении фото профиля с сервера:", error);
+    document.getElementById("profile-photo").innerText =
+      "Произошла ошибка при получении фото профиля.";
+  }
 }
 
 // Главная функция для загрузки фото профиля
 async function loadProfilePhoto() {
-    const isPhotoLoadedFromLocal = loadPhotoFromLocal();
+  const isPhotoLoadedFromLocal = loadPhotoFromLocal();
 
-    if (!isPhotoLoadedFromLocal) {
-        console.log('Фото не найдено в локальном хранилище, загружаем с сервера...');
-        await loadImageFromServer(); // Загружаем с сервера и сохраняем, если не найдено в локальном хранилище
-    } else {
-        console.log('Фото загружено из локального хранилища.');
-    }
+  if (!isPhotoLoadedFromLocal) {
+    console.log(
+      "Фото не найдено в локальном хранилище, загружаем с сервера..."
+    );
+    await loadImageFromServer(); // Загружаем с сервера и сохраняем, если не найдено в локальном хранилище
+  } else {
+    console.log("Фото загружено из локального хранилища.");
+  }
 }
 
 // Инициализация загрузки фото
-(async function() {
-    await loadProfilePhoto();
+(async function () {
+  await loadProfilePhoto();
 })();
 
 // Рисование изображения на канвасе
 function drawImageToCanvas(image) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const imageRatio = image.width / image.height;
-    const maxWidth = window.innerWidth - 40;
-    const maxHeight = window.innerHeight * 0.8;
+  const imageRatio = image.width / image.height;
+  const maxWidth = window.innerWidth - 40;
+  const maxHeight = window.innerHeight * 0.8;
 
-    let canvasWidth, canvasHeight;
-    
-    if (imageRatio > 1) {
-        canvasWidth = Math.min(maxWidth, image.width);
-        canvasHeight = canvasWidth / imageRatio;
-    } else {
-        canvasHeight = Math.min(maxHeight, image.height);
-        canvasWidth = canvasHeight * imageRatio;
-    }
+  let canvasWidth, canvasHeight;
 
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
+  if (imageRatio > 1) {
+    canvasWidth = Math.min(maxWidth, image.width);
+    canvasHeight = canvasWidth / imageRatio;
+  } else {
+    canvasHeight = Math.min(maxHeight, image.height);
+    canvasWidth = canvasHeight * imageRatio;
+  }
 
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
 
-    imageLoaded = true;
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+  imageLoaded = true;
 }
 
 // Функция для получения насыщенности пикселя
 function getSaturation(r, g, b) {
-    const max = Math.max(r, g, b);
-    const min = Math.min(r, g, b);
-    return max - min;
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+  return max - min;
 }
 
 // Пикселизация изображения с сеткой
 function pixelateWithGrid() {
-    if (!imageLoaded) return;
+  if (!imageLoaded) return;
 
-    const pixelSize = parseInt(pixelSizeInput.value, 10);
-    
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
+  const pixelSize = parseInt(pixelSizeInput.value, 10);
 
-    for (let y = 0; y < canvas.height; y++) {
-        for (let x = 0; x < canvas.width; x++) {
-            const index = (y * canvas.width + x) * 4;
-            const r = data[index];
-            const g = data[index + 1];
-            const b = data[index + 2];
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
 
-            if (x < canvas.width - 1 && y < canvas.height - 1) {
-                const neighborIndex = ((y + 1) * canvas.width + (x + 1)) * 4;
-                const neighborR = data[neighborIndex];
-                const neighborG = data[neighborIndex + 1];
-                const neighborB = data[neighborIndex + 2];
+  for (let y = 0; y < canvas.height; y++) {
+    for (let x = 0; x < canvas.width; x++) {
+      const index = (y * canvas.width + x) * 4;
+      const r = data[index];
+      const g = data[index + 1];
+      const b = data[index + 2];
 
-                const currentSaturation = getSaturation(r, g, b);
-                const neighborSaturation = getSaturation(neighborR, neighborG, neighborB);
+      if (x < canvas.width - 1 && y < canvas.height - 1) {
+        const neighborIndex = ((y + 1) * canvas.width + (x + 1)) * 4;
+        const neighborR = data[neighborIndex];
+        const neighborG = data[neighborIndex + 1];
+        const neighborB = data[neighborIndex + 2];
 
-                if (Math.abs(currentSaturation - neighborSaturation) > 50) {
-                    const maxR = Math.max(r, neighborR);
-                    const maxG = Math.max(g, neighborG);
-                    const maxB = Math.max(b, neighborB);
+        const currentSaturation = getSaturation(r, g, b);
+        const neighborSaturation = getSaturation(
+          neighborR,
+          neighborG,
+          neighborB
+        );
 
-                    data[index] = maxR;
-                    data[index + 1] = maxG;
-                    data[index + 2] = maxB;
-                }
-            }
+        if (Math.abs(currentSaturation - neighborSaturation) > 50) {
+          const maxR = Math.max(r, neighborR);
+          const maxG = Math.max(g, neighborG);
+          const maxB = Math.max(b, neighborB);
+
+          data[index] = maxR;
+          data[index + 1] = maxG;
+          data[index + 2] = maxB;
         }
+      }
     }
+  }
 
-    ctx.putImageData(imageData, 0, 0);
+  ctx.putImageData(imageData, 0, 0);
 
-    for (let y = 0; y < canvas.height; y += pixelSize) {
-        for (let x = 0; x < canvas.width; x += pixelSize) {
-            const index = (y * canvas.width + x) * 4;
-            const red = data[index];
-            const green = data[index + 1];
-            const blue = data[index + 2];
+  for (let y = 0; y < canvas.height; y += pixelSize) {
+    for (let x = 0; x < canvas.width; x += pixelSize) {
+      const index = (y * canvas.width + x) * 4;
+      const red = data[index];
+      const green = data[index + 1];
+      const blue = data[index + 2];
 
-            ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
-            ctx.fillRect(x, y, pixelSize, pixelSize);
-        }
+      ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+      ctx.fillRect(x, y, pixelSize, pixelSize);
     }
+  }
 }
 
 // Обработчик изменения размера пикселя
-pixelSizeInput.addEventListener('input', () => {
-    pixelSizeValue.textContent = pixelSizeInput.value;
-    if (imageLoaded) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawImageToCanvas(img);
-        pixelateWithGrid();
-    }
+pixelSizeInput.addEventListener("input", () => {
+  pixelSizeValue.textContent = pixelSizeInput.value;
+  if (imageLoaded) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawImageToCanvas(img);
+    pixelateWithGrid();
+  }
 });
 
 // Обработка загрузки изображения вручную
-document.getElementById('upload-image').addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            img.src = e.target.result;
-            img.onload = () => {
-                canvas.style.borderRadius = '0';
-                drawImageToCanvas(img);
-                pixelateWithGrid();
-            };
-        };
-        reader.readAsDataURL(file);
-    }
+document.getElementById("upload-image").addEventListener("change", (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      img.src = e.target.result;
+      img.onload = () => {
+        canvas.style.borderRadius = "0";
+        drawImageToCanvas(img);
+        pixelateWithGrid();
+      };
+    };
+    reader.readAsDataURL(file);
+  }
 });
 
 const initData = Telegram.WebApp.initData;
@@ -215,47 +226,50 @@ const initDataUnsafe = Telegram.WebApp.initDataUnsafe || {};
 
 // Отправка изображения на сервер
 async function uploadImageToServer() {
-    const canvas = document.getElementById('canvas');
-    const val = { text: 'image-name' }; 
+  const canvas = document.getElementById("canvas");
+  const val = { text: "image-name" };
 
-    // Преобразуем содержимое canvas в Blob (файл)
-    canvas.toBlob(async (blob) => {
-        if (!blob) {
-            alert('Ошибка при создании изображения.');
-            return;
-        }
+  // Преобразуем содержимое canvas в Blob (файл)
+  canvas.toBlob(async (blob) => {
+    if (!blob) {
+      alert("Ошибка при создании изображения.");
+      return;
+    }
 
-        // Создаем FormData для отправки файла на сервер
-        const formData = new FormData();
-        formData.append('image', blob, `${val.text}.jpg`); 
+    // Создаем FormData для отправки файла на сервер
+    const formData = new FormData();
+    formData.append("image", blob, `${val.text}.jpg`);
 
-        try {
-            // Отправляем изображение на сервер
-            const response = await fetch('/uploadImage', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `initData ${initData}`, 
-                },
-                body: formData // Отправляем файл с помощью FormData
-            });
+    try {
+      // Отправляем изображение на сервер
+      const response = await fetch("/uploadImage", {
+        method: "POST",
+        headers: {
+          Authorization: `initData ${initData}`,
+        },
+        body: formData, // Отправляем файл с помощью FormData
+      });
 
-            const data = await response.json();
+      const data = await response.json();
 
-            if (response.ok && data.success) {
-                console.log('Изображение успешно загружено на сервер:', data.fileUrl);
-                Telegram.WebApp.openLink(data.fileUrl); // Открываем ссылку на изображение через Telegram.WebApp
-            } else {
-                console.error('Ошибка при загрузке изображения:', data.message || 'Неизвестная ошибка');
-                alert('Ошибка: ' + (data.message || 'Неизвестная ошибка'));
-            }
-        } catch (error) {
-            console.error('Ошибка при отправке изображения:', error);
-            alert('Ошибка при отправке изображения.');
-        }
-    }, 'image/jpeg'); 
+      if (response.ok && data.success) {
+        console.log("Изображение успешно загружено на сервер:", data.fileUrl);
+        Telegram.WebApp.openLink(data.fileUrl); // Открываем ссылку на изображение через Telegram.WebApp
+      } else {
+        console.error(
+          "Ошибка при загрузке изображения:",
+          data.message || "Неизвестная ошибка"
+        );
+        alert("Ошибка: " + (data.message || "Неизвестная ошибка"));
+      }
+    } catch (error) {
+      console.error("Ошибка при отправке изображения:", error);
+      alert("Ошибка при отправке изображения.");
+    }
+  }, "image/jpeg");
 }
 
 // Обработка нажатия на кнопку отправки
-document.getElementById('send-image').addEventListener('click', uploadImageToServer);
-
-
+document
+  .getElementById("send-image")
+  .addEventListener("click", uploadImageToServer);
