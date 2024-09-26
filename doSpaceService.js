d3e0426a7795adb6d49c118527668c7f70cf945e
TUGITA <tugitaa@gmail.com>
2024-09-26 13:53:44 +0000

    doSpaceService

--- doSpaceService.js
+++ doSpaceService.js
@@ -18,51 +18,34 @@ function getS3() {
     });
 }
 
-// Функция для загрузки оригинального файла на DigitalOcean Spaces
-async function uploadOriginalFile(userId, file) {
-    const ext = '.jpg'; // Фиксированное расширение
-    const fileName = `${userId}${ext}`; // Имя файла — userId.jpg
-    const finalPath = `api-clicker/tg/avatars/${fileName}`; // Путь к файлу
-
-    const s3 = getS3();
-    const params = {
-        Bucket: 'cobuild',
-        Key: finalPath,
-        Body: file.buffer,
-        ACL: 'public-read',
-        ContentType: file.mimetype || 'image/jpeg',
-    };
-
-    try {
-        await s3.putObject(params).promise();
-        return `https://cdn.joincommunity.xyz/${finalPath}`;
-    } catch (error) {
-        throw new Error(`Ошибка загрузки оригинального файла: ${error.message}`);
-    }
-}
-
 // Функция для загрузки пикселизированного файла на DigitalOcean Spaces
-async function uploadPixelatedFile(userId, pixelatedBuffer) {
-    const ext = '.jpg'; // Фиксированное расширение
-    const fileName = `${userId}${ext}`; // Имя файла — userId.jpg
-    const finalPath = `api-clicker/tg/avatars/pixel/${fileName}`; // Путь к пикселизированному файлу
-
+async function uploadPixelatedFile(userId, pixelatedBuffer, expire) {
     const s3 = getS3();
+    const ext = '.jpg'; // Фиксированное расширение
+    const fileName = `${userId}_${Date.now()}${ext}`; // Генерация уникального имени файла
+    const finalPath = `api-clicker/tg/avatars/pixel/${fileName}`; // Уникальный путь к пикселизированному файлу
+
     const params = {
-        Bucket: 'cobuild',
+        Bucket: 'cobuild', // Ваш Bucket в DigitalOcean Spaces
         Key: finalPath,
         Body: pixelatedBuffer,
         ACL: 'public-read',
         ContentType: 'image/jpeg',
     };
 
+    if (expire) {
+        params.Expires = new Date(expire); // Устанавливаем время истечения срока действия
+    }
+
     try {
+        // Загружаем файл
         await s3.putObject(params).promise();
         return `https://cdn.joincommunity.xyz/${finalPath}`;
     } catch (error) {
         throw new Error(`Ошибка загрузки пикселизированного файла: ${error.message}`);
     }
 }
+
 
 
 module.exports = {
